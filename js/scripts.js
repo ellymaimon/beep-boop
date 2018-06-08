03745372f1db17ffa37fbf4fcca44768b2abed90
Elly Maimon <eliranmaimon@gmail.com>
2018-06-08 10:18:05 -0700

    add functions to check for ones and zeros

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
 //Business Logic
+
+//Function that creates the pattern to be displayed to the user
 function createPattern(number) {
   var sorry = "I'm sorry Dave, I'm afraid I can't do that."
-  var zeros = "Beep!"
-  var ones = "Boop!"
-  var numberArray = number.toString().split("");
-  console.log(numberArray);
+  var beep = "Beep!" //zeros
+  var boop = "Boop!" //ones
 
   for (i = 0; i <= number; i++) {
     if (i % 3 === 0) {
-
+      return sorry;
+    } else if (hasOnes(i)) {
+      return boop;
+    } else if (hasZeros(i)) {
+      return beep;
     }
   }
 
+}
+
+//Function that checks for ones
+function hasOnes(numberInLoop) {
+  var digits = numberInLoop.toString.split("");
+  digits.forEach(function(digit) {
+    if (digit === 0) {
+      return true;
+    }
+  });
+}
+
+//Function that checks for zeros
+function hasZeros(numberInLoop) {
+  var digits = numberInLoop.toString.split("");
+  digits.forEach(function(digit) {
+    if (digit === 1) {
+      return true;
+    }
+  });
 }
 
 
@@ -19,25 +43,13 @@ function createPattern(number) {
 //UI Logic
 $(function() {
   $("button#submit").click(function() {
-
-
     var initialNumber = parseInt($("#input-number").val());
-
-
     console.log(initialNumber);
-
-
     $(".results").show();
-
-
     var finalAnswer = createPattern(initialNumber);
-
-
     $("#final-result").text(finalAnswer);
   });
 });
-
-
 
 // Numbers that contain a 0 are replaced (all digits) with "Beep!"
 // Numbers that contain a 1 are replaced (all digits) with "Boop!"
