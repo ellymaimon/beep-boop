4638b8c282b04b301fa09ac50246283a180ef3ae
Elly Maimon <eliranmaimon@gmail.com>
2018-06-08 08:52:22 -0700

    update file structure and add html page structure

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//Business Logic
+
+
+
+
+//UI Logic
+$(function() {
+  $("")
+});
+
+
+
+// Numbers that contain a 0 are replaced (all digits) with "Beep!"
+// Numbers that contain a 1 are replaced (all digits) with "Boop!"
+// Numbers divisible by 3 are replaced with "I'm sorry, Dave. I'm
+// afraid I can't do that."
